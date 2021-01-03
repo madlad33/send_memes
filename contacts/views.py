76bc01e834f62a0ae633dcfe315c63cc389a5096
Tanmay <tanmay.milky33@gmail.com>
2021-01-03 20:07:26 +0530

    installed twilio and added views

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView,FormView,DetailView
+from .models import Contact
+from .forms import ContactForm,MessageForm
+from django.urls import reverse_lazy
+from django.http import JsonResponse
+from django.shortcuts import redirect
+
+class ContactListView(ListView):
+    model = Contact
+    template_name = 'contacts/main.html'
+
+
+class MessageDetailView(DetailView):
+    model = Contact
+    template_name = 'contacts/detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = MessageForm
+        return context
+    def post(self,request,*args,**kwargs):
+        form = MessageForm(request.POST)
+        if request.is_ajax():
+            if form.is_valid():
+                message_to = self.get_object()
+                body = form.cleaned_data.get('body')
+                send_whatsapp_message(body,message_to)
+                return JsonResponse({"body":body})
+            return redirect('contact-list')
+        return redirect('contact-list')
+
+
+
+class AddContactView(FormView):
+    form_class = ContactForm
+    template_name = 'contacts/add.html'
+    success_url = reverse_lazy('contact-list')
