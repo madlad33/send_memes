2ee5400aae815d5bb182ed1c6faf0d4bb62ae7d0
Tanmay <tanmay.milky33@gmail.com>
2021-01-03 19:35:36 +0530

    registered model to admin and added forms

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Contact
+# Register your models here.
+admin.site.register(Contact)
